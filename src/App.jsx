2ff1b8b0db3acd556db0ef1702c9ce1b5ec7f78c
komaldhillon08 <komaldhillonpb19@gmail.com>
2025-09-10 14:55:40 +0530

    create the file like home about etc

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header"
+function App() {
+  return (
+    <div className="container border-1 mx-auto">
+      <Header/> 
+    </div>
+  )
+}
+export default App
---
+++ src/product.js
@@ -0,0 +1,45 @@
+import shoe1  from "../public/shoe1.jpg"
+import shoe2  from "../public/shoe2.jpg"
+import shoe3  from "../public/shoe3.jpg"
+import shoe4  from "../public/shoe4.jpg"
+import shoe5  from "../public/shoe5.jpg"
+import shoe6  from "../public/shoe6.jpg"
+
+export const product =   [
+    {
+        "id" : "1" , 
+        "productImg" : {shoe1} ,
+        "productName" : "item - 1" ,
+        "price" : "10"
+    } , 
+     {
+        "id" : "2" , 
+        "productImg" : {shoe2} ,
+        "productName" : "item - 2" ,
+        "price" : "2.66"
+    } ,
+    {
+        "id" : "3" , 
+        "productImg" : {shoe3} ,
+        "productName" : "item - 3" ,
+        "price" : "3"
+    } ,
+    {
+        "id" : "4" , 
+        "productImg" : {shoe4} ,
+        "productName" : "item - 4" ,
+        "price" : "4.50"
+    } ,
+    {
+        "id" : "5" , 
+        "productImg" : {shoe5} ,
+        "productName" : "item - 5" ,
+        "price" : "5"
+    } ,
+    {
+        "id" : "6" , 
+        "productImg" : {shoe6} ,
+        "productName" : "item - 6" ,
+        "price" : "3.33"
+    } ,
+]
